1b849530bbef1611f19ec20220b248ff7c16bcb4
Diya Punetha <diyapunetha333@gmail.com>
2025-07-02 22:21:09 +0530

    2-07-2025

---
+++ Return the next number from the integer passed.py
@@ -0,0 +1,15 @@
+"""
+Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
+Examples
+addition(0) ➞ 1
+
+addition(9) ➞ 10
+
+addition(-3) ➞ -2
+"""
+def addition(num):
+	i=num+1
+	return i
+result=addition(0)
+print(result)
