1af6de8a63ef7b90361c5550de14221f04313052
Diya Punetha <diyapunetha333@gmail.com>
2025-07-02 22:15:29 +0530

    2-07-2025

---
+++ Return te sum of two numbers.py
@@ -0,0 +1,14 @@
+"""Create a function that takes two numbers as arguments and returns their sum.
+
+Examples
+addition(3, 2) ➞ 5
+
+addition(-3, -6) ➞ -9
+
+addition(7, 3) ➞ 10 """
+
+def addition(a,b):
+    c=a+b
+    return c
+result=addition(3,2)
+print(result)
