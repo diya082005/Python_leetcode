26cf9cf7a1c090ce0e2926d4c23e6138a4764114
Diya Punetha <diyapunetha333@gmail.com>
2025-07-05 22:11:02 +0530

    5-07-2025

---
+++ convert age to days.py
@@ -0,0 +1,12 @@
+"""
+Create a function that takes the age in years and returns the age in days.
+
+Examples
+calc_age(65) ➞ 23725
+
+calc_age(0) ➞ 0
+
+calc_age(20) ➞ 7300
+"""
+def calc_age(age):
+	return age*365
