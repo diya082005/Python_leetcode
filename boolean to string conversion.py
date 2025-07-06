ceaac4398f5cafc5992132dbe44cbaa50b20dac1
Diya Punetha <diyapunetha333@gmail.com>
2025-07-06 11:58:35 +0530

    6-07-2025

---
+++ boolean to string conversion.py
@@ -0,0 +1,13 @@
+"""
+Create a function that takes a boolean variable flag and returns it as a string.
+
+Examples
+bool_to_string(True) ➞ "True"
+
+bool_to_string(False) ➞ "False"
+"""
+def bool_to_string(flag):
+    return str(flag)
+
+print(bool_to_string(True))   # ➞ "True"
+print(bool_to_string(False))  # ➞ "False"
