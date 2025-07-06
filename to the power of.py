3b9a0dc93c62310dfeb732577c20504312f6ab39
Diya Punetha <diyapunetha333@gmail.com>
2025-07-06 11:54:21 +0530

    6-07-2025

---
+++ to the power of.py
@@ -0,0 +1,13 @@
+"""
+Create a function that takes a base number and an exponent number and returns the calculation.
+
+Examples
+calculate_exponent(5, 5) ➞ 3125
+
+calculate_exponent(10, 10) ➞ 10000000000
+
+calculate_exponent(3, 3) ➞ 27
+
+"""
+def calculate_exponent(num, exp):
+	return num ** exp
